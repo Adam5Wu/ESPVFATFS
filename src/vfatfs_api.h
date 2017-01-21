0723bed0df4feafb6dca5cf225459a18cb046f9e
Zhenyu Wu <Adam_5Wu@hotmail.com>
2017-01-21 02:31:03 -0500

    Remove libc header, trying to solve CI problem

--- src/vfatfs_api.h
+++ src/vfatfs_api.h
@@ -22,7 +22,7 @@
 	License along with this library; if not, write to the Free Software
 	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  */
-#include <limits>
+//#include <limits>
 #include "FS.h"
 
 #include "FSImpl.h"
